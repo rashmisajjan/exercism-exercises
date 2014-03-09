ec3df5d8120f7b680de83e2cb14d99b3522c9e6c
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-09 23:15:56 +0100

    python/bob: Discovered unexpected funcitonality of the str.isupper method. Could remove is_yelling method

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -5,22 +5,8 @@ class Bob:
         "Bob's response to anything"
         if not says_the_other or says_the_other.isspace():
             return 'Fine. Be that way!'
-        elif self.is_yelling(says_the_other):
+        elif says_the_other.isupper():
             return 'Woah, chill out!'
         elif says_the_other[-1] == '?':
             return 'Sure.'
         return 'Whatever.'
-
-    @staticmethod
-    def is_yelling(text):
-        "Determines whether text contains yelling or not"
-        letters = ''.join([c for c in text if c.isalpha()])
-        if letters and letters.isupper():   # YELLING
-            return True
-        if text[-1] == '!':
-            if isinstance(text, unicode):   # Just a tourist
-                return False
-            if "make out" in text:  # Sounds like a nice proposition!
-                return False
-            return True     # The exclamation mark marks the end of yelling!
-        return False
