77825d1b1cdced3ff7fd2c8fa6fae1c3e3c01714
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-10 03:41:14 +0100

    haskell/rna-transcription: Initial solution

---
+++ haskell/rna-transcription/DNA.hs
@@ -0,0 +1,9 @@
+module DNA (toRNA) where
+
+-- | toRNA returns the RNA complement for a given DNA strand.
+toRNA :: String -> String
+toRNA = map f
+  where f 'G' = 'C'
+        f 'C' = 'G'
+        f 'T' = 'A'
+        f 'A' = 'U'
