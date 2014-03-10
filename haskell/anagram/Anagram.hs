5470388a6f9fafafc4e02b7fcaaf42c77bb05fbd
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-10 23:02:13 +0100

    haskell/anagram: Increased density, added comments.

--- haskell/anagram/Anagram.hs
+++ haskell/anagram/Anagram.hs
@@ -1,4 +1,4 @@
-module Anagram ( anagramsFor ) where
+module Anagram (anagramsFor) where
 
 
 import Data.Char (toLower)
@@ -9,14 +9,16 @@ import Data.List (sort)
 -- returns the list of those candidates that are anagrams of the word.
 anagramsFor :: String -> [String] -> [String]
 anagramsFor x = filter isAnagramOfX
-  where isAnagramOfX y
-          | yChars /= xChars = False  -- must have the same characters
-          | yLower == xLower = False  -- can't just be differently cased
-          | otherwise        = True
-          where yChars = sort yLower
-                yLower = lowerCase y
-        xLower = lowerCase x
-        xChars = sort xLower
+  where
+    -- 'isAnagramOfX' checks that
+    -- 1) y isn't simply a differently cased version of x
+    -- 2) y consists of the same characters as x, irregardless of the
+    -- casing.
+    isAnagramOfX y = yLower /= xLower && yChars == xChars
+      where yLower = lowerCase y
+            yChars = sort yLower
+    xLower = lowerCase x
+    xChars = sort xLower
 
 
 lowerCase :: String -> String
