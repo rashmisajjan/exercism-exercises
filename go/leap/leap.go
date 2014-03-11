94973942ff5e812a4f62f800d169756269addf05
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-12 00:02:43 +0100

    go/leap: improved readability

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,10 @@
 package leap
 
 func IsLeapYear(year int) bool {
-	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			} else {
-				return false
-			}
-		} else {
-			return true
-		}
-	} else {
-		return false
+
+	divisibleBy := func(d int) bool {
+		return year%d == 0
 	}
+
+	return divisibleBy(4) && (!divisibleBy(100) || divisibleBy(400))
 }
