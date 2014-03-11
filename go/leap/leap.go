4da81267c86d778913a0fbaf1aed3c3da984c25a
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-11 15:58:59 +0100

    go/leap: Untested first try

---
+++ go/leap/leap.go
@@ -0,0 +1,17 @@
+package leap
+
+func IsLeapYear(year int) bool {
+        if year % 4 == 0 {
+                if year % 100 == 0 {
+                        if year % 400 == 0 {
+                                return true
+                        } else {
+                                return false
+                        }
+                } else {
+                        return true
+                }
+        } else {
+                return false
+        }
+}
