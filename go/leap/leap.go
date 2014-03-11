afd38872c40d6a3f5e0fa92549d031cf8d9ceaa0
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-11 20:24:21 +0100

    go/leap: gofmt'd, tests pass.

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,17 @@
 package leap
 
 func IsLeapYear(year int) bool {
-        if year % 4 == 0 {
-                if year % 100 == 0 {
-                        if year % 400 == 0 {
-                                return true
-                        } else {
-                                return false
-                        }
-                } else {
-                        return true
-                }
-        } else {
-                return false
-        }
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			} else {
+				return false
+			}
+		} else {
+			return true
+		}
+	} else {
+		return false
+	}
 }
