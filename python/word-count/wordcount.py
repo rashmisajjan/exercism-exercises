9b8c185a69df095537aa51145f4cbd96869219d2
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-12 11:35:44 +0100

    python/word-count: improved docstrings

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -3,6 +3,7 @@ import re
 
 
 class Phrase(object):
+    """Represent a phrase."""
 
     __word_re = re.compile(r"\w+", re.UNICODE)
 
@@ -10,12 +11,14 @@ class Phrase(object):
         self.phrase = phrase
 
     def words(self):
-        """Return an iterator of the words in the phrase."""
+        """Return an iterator of the words, i.e. sequences composed of
+        alphanumeric characters and underscores ("_"), in the phrase.
+        """
         return (m.group(0) for m in self.__word_re.finditer(self.phrase))
 
     def word_count(self):
-        """Return a dictionary where the keys are the words in the phrase,
-        normalized to lower case, and the values are the frequency of these
-        words in the phrase.
+        """Return a dict (a Counter, really) where the keys are the
+        words in the phrase, normalized to lower case, and the values are
+        the frequencies of these words in the phrase.
         """
         return Counter(word.lower() for word in self.words())
