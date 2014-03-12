d48e8e0ef94833b14d9999bfdec8ab9078dbcc1f
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-12 11:11:36 +0100

    python/word-count: regex constant pulled into class

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,17 +1,17 @@
 from collections import Counter
 import re
 
-WORD_REGEX = re.compile(r"\w+", re.UNICODE)
-
 
 class Phrase(object):
+
+    __word_re = re.compile(r"\w+", re.UNICODE)
 
     def __init__(self, phrase):
         self.phrase = phrase
 
     def words(self):
         """Return an iterator of the words in the phrase."""
-        return (m.group(0) for m in WORD_REGEX.finditer(self.phrase))
+        return (m.group(0) for m in self.__word_re.finditer(self.phrase))
 
     def word_count(self):
         """Return a dictionary where the keys are the words in the phrase,
