ee5b31f54e80ef4b05c20115317ee76ca7a24321
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-12 18:17:52 +0100

    python/anagram: Enhanced symmetry.

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -3,18 +3,14 @@ class Anagram(object):
     def __init__(self, word):
         self.word = word
         self._lower = word.lower()
-        self._normalized = self.__normalize(self._lower)
+        self._signature = sorted(self._lower)
 
-    @staticmethod
-    def __normalize(word):
-        """Return the letters of a word sorted and lowercased."""
-        return sorted(word.lower())
+    def is_anagram_to(self, other):
+        """Check if self and other are anagrams."""
+        return ((other._signature == self._signature) and
+                (other._lower != self._lower))
 
-    def is_anagram(self, candidate):
-        """Check if candidate is an anagram of self.word."""
-        return ((self.__normalize(candidate) == self._normalized) and
-                (candidate.lower() != self._lower))
-
-    def match(self, candidates):
-        """Return only those candidates that are anagrams of self.word."""
-        return [c for c in candidates if self.is_anagram(c)]
+    def match(self, candidate_words):
+        """Return only those candidate words that are anagrams of self.word."""
+        candidate_anagrams = (Anagram(c) for c in candidate_words)
+        return [a.word for a in candidate_anagrams if self.is_anagram_to(a)]
