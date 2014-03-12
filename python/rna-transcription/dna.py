aaec33e587a9773e5bf8503e4850510ab8e5eb98
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-12 16:18:15 +0100

    python/rna-transcription: Clean up, better naming.

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,14 +1,14 @@
-import string
+from string import maketrans
 
 
 class DNA(object):
     """Represent a strand of DNA."""
 
-    DNA_RNA_TRANSLATION_TABLE = string.maketrans('GCTA', 'CGAU')
+    __rna_translation_table = maketrans('GCTA', 'CGAU')
 
     def __init__(self, dna_string):
         self.dna_string = dna_string
 
     def to_rna(self):
         """Return the RNA complement of the DNA strand."""
-        return self.dna_string.translate(self.DNA_RNA_TRANSLATION_TABLE)
+        return self.dna_string.translate(self.__rna_translation_table)
