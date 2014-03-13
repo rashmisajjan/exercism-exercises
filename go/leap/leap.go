118d5d09e700d4cb3451462d552698381077fd10
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-13 01:16:16 +0100

    go/leap: Shortened

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,10 +1,5 @@
 package leap
 
 func IsLeapYear(year int) bool {
-
-	divisibleBy := func(d int) bool {
-		return year%d == 0
-	}
-
-	return divisibleBy(4) && (!divisibleBy(100) || divisibleBy(400))
+	return year%4 == 0 && (!(year%100 == 0) || year%400 == 0)
 }
