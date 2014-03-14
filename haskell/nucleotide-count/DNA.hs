fcf8cdd453937d1f5fa6d5cbffdd2cb1ba504d54
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-14 17:25:05 +0100

    haskell/nucleotide-count: More concise solution for 'count'.

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -3,16 +3,19 @@ module DNA
     , nucleotideCounts
     ) where
 
-import Data.Map ( Map, (!), adjust, fromList, member )
+
+import Data.Map ( Map, adjust, fromList, lookup )
+import Data.Maybe ( fromMaybe)
 import Data.Word ( Word )
+import Prelude hiding ( lookup )
+
 
 count :: Char -> String -> Word
-count nucleotide strand
-  | nucleotide `member` counts = counts ! nucleotide
-  | nucleotide == 'U'          = 0
-  | otherwise                  = error errorMsg
-  where counts = nucleotideCounts strand
-        errorMsg = "invalid nucleotide " ++ '\'' : nucleotide : "'"
+count nucleotide strand =
+    fromMaybe (if nucleotide == 'U' then 0 else error msg)
+              (nucleotide `lookup` nucleotideCounts strand)
+  where msg = "invalid nucleotide " ++ '\'' : nucleotide : "'"
+
 
 nucleotideCounts :: String -> Map Char Word
 nucleotideCounts = foldr (adjust (+1)) (fromList $ zip "ACGT" [0, 0, 0, 0])
