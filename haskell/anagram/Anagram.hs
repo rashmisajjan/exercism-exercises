4b16b63ceededa45c981e7cc545bd8b6eb056aea
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-14 02:29:17 +0100

    haskell/anagram: Improved structure, more concise.

--- haskell/anagram/Anagram.hs
+++ haskell/anagram/Anagram.hs
@@ -8,17 +8,10 @@ import Data.List (sort)
 -- | 'anagramsFor', applied to a word and a list of candidate strings,
 -- returns the list of those candidates that are anagrams of the word.
 anagramsFor :: String -> [String] -> [String]
-anagramsFor x = filter isAnagramOfX
-  where
-    -- 'isAnagramOfX' checks that
-    -- 1) y isn't simply a differently cased version of x
-    -- 2) y consists of the same characters as x, irregardless of the
-    -- casing.
-    isAnagramOfX y = yLower /= xLower && yChars == xChars
-      where yLower = lowerCase y
-            yChars = sort yLower
-    xLower = lowerCase x
-    xChars = sort xLower
+anagramsFor x ys = filter (isAnagramOfX . lowerCase) ys
+  where isAnagramOfX yLower = yLower /= xLower && sort yLower == xLowerSorted
+        xLower              = lowerCase x
+        xLowerSorted        = sort xLower
 
 
 lowerCase :: String -> String
