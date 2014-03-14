f7e9fbe46a2635d2cce80be8cfd9ce05f3f9a137
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-14 17:41:22 +0100

    haskell/nucleotide-count: Types for readability.

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -10,12 +10,16 @@ import Data.Word ( Word )
 import Prelude hiding ( lookup )
 
 
-count :: Char -> String -> Word
-count nucleotide strand =
-    fromMaybe (if nucleotide == 'U' then 0 else error msg)
-              (nucleotide `lookup` nucleotideCounts strand)
-  where msg = "invalid nucleotide " ++ '\'' : nucleotide : "'"
+type Nucleotide = Char
+type DnaStrand  = [Nucleotide]
+type Count      = Word
 
 
-nucleotideCounts :: String -> Map Char Word
+count :: Nucleotide -> DnaStrand -> Count
+count nuc strand = fromMaybe (if nuc == 'U' then 0 else error msg)
+                             (nuc `lookup` nucleotideCounts strand)
+  where msg = "invalid nucleotide " ++ '\'' : nuc : "'"
+
+
+nucleotideCounts :: DnaStrand -> Map Nucleotide Count
 nucleotideCounts = foldr (adjust (+1)) (fromList $ zip "ACGT" [0, 0, 0, 0])
