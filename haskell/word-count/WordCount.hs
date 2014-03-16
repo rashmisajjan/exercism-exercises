bcae1815682d68853f048d9000e7cb87010042b2
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-16 01:47:07 +0100

    haskell/word-count: Modularized and slightly improved.

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,18 +1,21 @@
 module WordCount ( wordCount ) where
 
 
-import Data.Char ( isDigit, isLetter, isSpace, toLower )
+import Data.Char ( isDigit, isLetter, toLower )
 import Data.Map  ( Map, fromListWith )
 import Data.Word ( Word )
+
+
+wordCount :: String -> Map String Word
+wordCount = counter . words . map normalize
+
+
+counter :: Ord a => [a] -> Map a Word
+counter = fromListWith (+) . flip zip (repeat 1)
 
 
 normalize :: Char -> Char
 normalize c
   | isLetter c = toLower c
   | isDigit c  = c
-  | isSpace c  = ' '
   | otherwise  = ' '
-
-
-wordCount :: String -> Map String Word
-wordCount = fromListWith (+) . flip zip (repeat 1) . words . map normalize
