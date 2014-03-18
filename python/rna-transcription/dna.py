f550db66e62b0ed070b4c6a3a338ff0244f203b1
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-18 22:57:35 +0100

    python/rna-transcription: Relocated the translation table.

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -4,11 +4,9 @@ from string import maketrans
 class DNA(object):
     """Represent a strand of DNA."""
 
-    __rna_translation_table = maketrans('GCTA', 'CGAU')
-
     def __init__(self, dna_string):
         self.dna_string = dna_string
 
-    def to_rna(self):
+    def to_rna(self, TRANSLATION_TABLE=maketrans('GCTA', 'CGAU')):
         """Return the RNA complement of the DNA strand."""
-        return self.dna_string.translate(self.__rna_translation_table)
+        return self.dna_string.translate(TRANSLATION_TABLE)
