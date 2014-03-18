057b8b4d3c5667d3a492afc88f4f328c14bbe331
Simon Jakobi <simon.jakobi@gmail.com>
2014-03-18 01:22:09 +0100

    haskell/nucleotide-count: Strict counts. Better looks.

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -4,7 +4,8 @@ module DNA
     ) where
 
 
-import Data.Map ( Map, adjust, fromList, lookup )
+import Data.List ( foldl' )
+import Data.Map.Strict ( Map, adjust, fromDistinctAscList, lookup )
 import Data.Maybe ( fromMaybe)
 import Data.Word ( Word )
 import Prelude hiding ( lookup )
@@ -16,10 +17,13 @@ type Count      = Word
 
 
 count :: Nucleotide -> DnaStrand -> Count
-count nuc strand = fromMaybe (if nuc == 'U' then 0 else error msg)
-                             (nuc `lookup` nucleotideCounts strand)
-  where msg = "invalid nucleotide " ++ '\'' : nuc : "'"
+count nuc = fromMaybe notFound . lookup nuc . nucleotideCounts
+  where notFound = case nuc of
+          'U' -> 0
+          _   -> error $ "invalid nucleotide " ++ show nuc
 
 
 nucleotideCounts :: DnaStrand -> Map Nucleotide Count
-nucleotideCounts = foldr (adjust (+1)) (fromList $ zip "ACGT" [0, 0, 0, 0])
+nucleotideCounts = foldl' increment zeroCounts
+  where increment counts nuc = adjust (+1) nuc counts
+        zeroCounts           = fromDistinctAscList [(c, 0) | c <- "ACGT"]
