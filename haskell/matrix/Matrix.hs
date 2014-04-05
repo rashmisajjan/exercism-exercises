06d8501a156f3ec749bd86576fa236c63a1fb4c1
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-05 21:48:47 +0200

    haskell/matrix: Reimplement Matrix with one-vector representation

--- haskell/matrix/Matrix.hs
+++ haskell/matrix/Matrix.hs
@@ -1,58 +1,43 @@
-module Matrix 
-    (
-      Matrix
-    , row
-    , column
-    , rows
-    , cols
-    , shape
-    , transpose
-    , reshape
-    , flatten
-    , fromString
-    , fromList
-    ) where
+module Matrix ( Matrix, flatten, rows, cols, row, column, shape
+              , reshape, transpose, fromList, fromString ) where 
 
 import Control.Arrow ( (&&&) )
+import Data.List ( unfoldr )
+import Data.Maybe ( listToMaybe )
 import qualified Data.Vector as V
+import Text.Printf ( printf )
 
-type Matrix a = V.Vector (V.Vector a)
+data Matrix a = Matrix { flatten :: V.Vector a
+                       , rows :: Int
+                       , cols :: Int
+                       } deriving (Eq, Show)
 
 row :: Int -> Matrix a -> V.Vector a
-row = flip (V.!)
+row i (Matrix cells _ nCols) = V.slice (i*nCols) nCols cells
 
 column :: Int -> Matrix a -> V.Vector a
-column i = row i . transpose
+column j m@(Matrix cells _ nCols) = V.backpermute cells indices
+  where indices = V.fromList [j, (j + nCols) .. (size m - 1)]
 
-rows :: Matrix a -> Int
-rows = V.length
-
-cols :: Matrix a -> Int
-cols = rows . transpose
+size :: Matrix a -> Int
+size = V.length . flatten
 
 shape :: Matrix a -> (Int, Int)
 shape = rows &&& cols
 
-transpose :: Matrix a -> Matrix a
-transpose m
-  | V.null m          = V.empty
-  | V.null (V.head m) = V.empty
-  | otherwise         = V.map V.head m `V.cons` transpose (V.map V.tail m)
-
 reshape :: (Int, Int) -> Matrix a -> Matrix a
-reshape (r, c) m = go r $ flatten m
-  where go 0  _ = V.empty
-        go r' v = let (h, t) = V.splitAt c v
-                  in  h `V.cons` go (r'-1) t
+reshape (nRows, nCols) m
+  | nRows * nCols == size m = m { rows=nRows, cols=nCols }
+  | otherwise = error $ printf "Can't reshape %s Matrix into %s Matrix."
+                               (show $ shape m) (show (nRows, nCols))
 
-flatten :: Matrix a -> V.Vector a
-flatten = V.concatMap id
+transpose :: Matrix a -> Matrix a
+transpose m@(Matrix _ nRows nCols) = Matrix cells' nCols nRows
+  where cells' = V.concat $ map (`column` m) [0 .. nCols-1]
 
 fromList :: [[a]] -> Matrix a
-fromList = V.fromList . map V.fromList
+fromList []         = Matrix V.empty 0 0
+fromList xss@(xs:_) = Matrix (V.fromList $ concat xss) (length xss) (length xs)
 
 fromString :: Read a => String -> Matrix a
-fromString = fromList . map readAll . lines
-  where readAll s = case reads s of
-          [] -> []
-          (x, rest) : _ -> x : readAll rest
+fromString = fromList . map (unfoldr (listToMaybe . reads)) . lines
