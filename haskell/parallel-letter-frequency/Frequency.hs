cc20921f3f4f88a4e6f4b25593c059502c562bae
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-07 21:49:15 +0200

    haskell/parallel-letter-frequency: Use parBuffer.

--- haskell/parallel-letter-frequency/Frequency.hs
+++ haskell/parallel-letter-frequency/Frequency.hs
@@ -2,20 +2,14 @@ module Frequency ( frequency ) where
 
 import qualified Control.Parallel.Strategies as S
 import Data.Char ( isLetter, toLower )
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 
 frequency :: Int -> [T.Text] -> M.Map Char Int
-frequency _ ts = M.unionsWith (+) $ eval $ concatMap (T.chunksOf 50) ts
-  where eval chunks = map frequency' chunks `S.using` S.parList S.rdeepseq
+frequency n = M.unionsWith (+)
+            . S.withStrategy (S.parBuffer n S.rdeepseq)
+            . map frequency'
 
 frequency' :: T.Text -> M.Map Char Int
 frequency' = T.foldl' f M.empty . T.filter isLetter
   where f counts char = M.insertWith (+) (toLower char) 1 counts
-
-
--- use
--- T.chunksOf
--- force (evalute fully)
--- parList
--- rdeepseq
