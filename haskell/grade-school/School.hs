b10786f1c614996d7f764c12c2235c72a4c8176f
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-07 09:22:55 +0200

    haskell/grade-school: First working solution.

---
+++ haskell/grade-school/School.hs
@@ -0,0 +1,21 @@
+module School ( School, empty, add, sorted, grade ) where
+
+import Data.List ( sort )
+import qualified Data.Map.Strict as M
+
+type Grade = Int
+type Student = String
+type School = M.Map Grade [Student]
+
+empty :: School
+empty = M.empty
+
+add :: Grade -> Student -> School -> School
+add grade student school = M.insertWith addToClass grade [student] school
+  where addToClass [new] students = sort (new : students)
+
+grade :: Grade -> School -> [Student]
+grade = M.findWithDefault []
+
+sorted :: School -> [(Grade, [Student])]
+sorted = M.toAscList
