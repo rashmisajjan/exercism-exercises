29054a2f9fa6c5f7413b6e3f8bc11cdc97c5e5e3
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-16 01:03:02 +0200

    haskell/raindrops: Replace concat by generator

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -1,7 +1,8 @@
 module Raindrops ( convert ) where
 
 convert :: Int -> String
-convert n | null pl_ngs = show n
-          | otherwise   = concat pl_ngs
-  where pl_ngs = [sound | (factor, sound) <- rain, n `rem` factor == 0]
-        rain = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+convert n | null sounds = show n
+          | otherwise   = sounds
+  where sounds = [c | (x, cs) <- [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+                    , n `rem` x == 0
+                    , c <- cs]
