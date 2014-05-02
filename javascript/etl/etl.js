5c935e913661df0c18106bfe84bfed8913113fe6
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-02 22:19:12 +0200

    javascript/etl: Improve performance with '+' instead of 'parseInt'

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -2,7 +2,7 @@ var transform = module.exports = function(scoresObj) {
   var result = {};
   Object.keys(scoresObj).forEach(function(score) {
     scoresObj[score].forEach(function(letter) {
-      result[letter.toLowerCase()] = parseInt(score);
+      result[letter.toLowerCase()] = +score;
     });
   });
   return result;
