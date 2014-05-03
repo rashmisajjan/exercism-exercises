648211345b621c98d43a2b376176e421701edc89
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-03 21:31:51 +0200

    javascript/etl: Unskip test and improve performance slightly

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,8 +1,9 @@
 var transform = module.exports = function(scoresObj) {
   var result = {};
-  Object.keys(scoresObj).forEach(function(score) {
-    scoresObj[score].forEach(function(letter) {
-      result[letter.toLowerCase()] = +score;
+  Object.keys(scoresObj).forEach(function(scoreStr) {
+    var score = +scoreStr;
+    scoresObj[scoreStr].forEach(function(letter) {
+      result[letter.toLowerCase()] = score;
     });
   });
   return result;
