af9b2160a79e4ccba6f3b2227404e376fd58156a
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-03 21:05:04 +0200

    javascript/bob: First *actually* working solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,12 +1,17 @@
 var Bob = module.exports = function() {
   
   this.hey = function(message) {
-    if (!message.trim) {
+
+    if (!message.trim()) {
       return 'Fine. Be that way!';
-    } else if (message === message.toUpperCase()) {
+
+    } else if (message === message.toUpperCase() &&
+               message !== message.toLowerCase()) {
       return 'Woah, chill out!';
-    } else if (message[message.length - 1] === '?') {
+
+    } else if (message.slice(-1) === '?') {
       return 'Sure.';
+
     } else {
       return 'Whatever.';
     };
