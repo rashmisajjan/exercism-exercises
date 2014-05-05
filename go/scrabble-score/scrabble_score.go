c57d6822574ced636b9067d7d27b19dff1ed10f7
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-05 10:35:43 +0200

    go/scrabble-score: First working solution

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,25 @@
+// Compute the Scrabble scores of strings
+
+package scrabble_score
+
+import "unicode"
+
+// points maps each letter of the English alphabet to their Scrabble score.
+var points = map[rune]int{
+	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1,
+	's': 1, 't': 1,
+	'd': 2, 'g': 2,
+	'b': 3, 'c': 3, 'm': 3, 'p': 3,
+	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+	'k': 5,
+	'j': 8, 'x': 8,
+	'q': 10, 'z': 10,
+}
+
+// Score computes the Scrabble score t for a given string s.
+func Score(s string) (t int) {
+	for _, r := range s {
+		t += points[unicode.ToLower(r)]
+	}
+	return
+}
