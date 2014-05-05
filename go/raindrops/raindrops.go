e363e0dfe1fa2438ef8ba9bf89cf5c888ba06b95
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-05 03:55:08 +0200

    go/raindrops: More simplicity and repetition

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,23 +2,20 @@ package raindrops
 
 import "strconv"
 
-var sounds = []struct {
-	factor int
-	sound  string
-}{
-	{3, "Pling"},
-	{5, "Plang"},
-	{7, "Plong"},
-}
-
 func Convert(n int) string {
 	output := ""
-	for _, s := range sounds {
-		if n%s.factor == 0 {
-			output += s.sound
-		}
+
+	if n%3 == 0 {
+		output += "Pling"
 	}
-	if len(output) > 0 {
+	if n%5 == 0 {
+		output += "Plang"
+	}
+	if n%7 == 0 {
+		output += "Plong"
+	}
+
+	if output != "" {
 		return output
 	}
 	return strconv.Itoa(n)
