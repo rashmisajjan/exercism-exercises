787ca886f563c1f46737437f76781ad67e1eb7f4
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-05 09:15:44 +0200

    go/grains: More comments and cleverness

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,12 +1,21 @@
+// Computations for a tale of a wise servant and an innumerate king
+//
+// There once was a wise servant who saved the life of a prince.
+// The king promised to pay whatever the servant could dream up.
+// Knowing that the king loved chess, the servant told the king he
+// would like to have grains of wheat. One grain on the first square
+// of a chess board. Two grains on the next. Four on the third, and so on.
+
 package grains
 
+// Square returns the number of grains on square n of the chess board.
 func Square(n int) uint64 {
-	return 1 << (uint64(n) - 1)
+	return 1 << uint(n-1)
 }
 
-func Total() (t uint64) {
-	for i := 1; i <= 64; i++ {
-		t += Square(i)
-	}
-	return
+// Total returns the total number of grains on the chessboard.
+func Total() uint64 {
+	// Total() is the sum of all powers of 2 with exponents 0 to 63.
+	// That sum is equal to 2^64 - 1.
+	return (1 << 64) - 1
 }
