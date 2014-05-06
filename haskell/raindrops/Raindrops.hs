41df11edf0a7bb72dfbeee0d64bd6058317a8ffe
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-06 10:32:30 +0200

    haskell/raindrops: concat for readability

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -1,8 +1,7 @@
-module Raindrops ( convert ) where
+module Raindrops (convert) where
 
 convert :: Int -> String
-convert n | null sounds = show n
-          | otherwise   = sounds
-  where sounds = [c | (x, cs) <- [(3, "Pling"), (5, "Plang"), (7, "Plong")]
-                    , n `rem` x == 0
-                    , c <- cs]
+convert n | null pl_ngs = show n
+          | otherwise   = concat pl_ngs
+  where pl_ngs = [sound | (factor, sound) <- rain, n `rem` factor == 0]
+        rain = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
