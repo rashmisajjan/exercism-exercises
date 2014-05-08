46319c59e3e496a710e56871526e53d93b560069
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-09 01:13:36 +0200

    haskell/meetup: Arithmetic solution

--- haskell/meetup/Meetup.hs
+++ haskell/meetup/Meetup.hs
@@ -1,7 +1,6 @@
 module Meetup (Weekday(..), Schedule(..), meetupDay) where
 
-import Data.Time.Calendar          (Day, fromGregorian,gregorianMonthLength,
-                                    toGregorian)
+import Data.Time.Calendar (Day, addDays, fromGregorian, gregorianMonthLength)
 import Data.Time.Calendar.WeekDate (toWeekDate)
 
 data Weekday = PlaceHolder | Monday | Tuesday | Wednesday
@@ -11,20 +10,25 @@ data Weekday = PlaceHolder | Monday | Tuesday | Wednesday
 data Schedule = First | Second | Third | Fourth | Last | Teenth
               deriving (Enum, Eq)
 
-selectWith :: Schedule -> [Day] -> Day
-selectWith Teenth   = head . filter ((> 12) . monthDay)
-selectWith Last     = last
-selectWith schedule = (!! fromEnum schedule)
-
-monthDay :: Day -> Int
-monthDay day = let (_, _, md) = toGregorian day
-               in md
-
 meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
-meetupDay schedule weekday year month
-    = selectWith schedule $ filter rightWeekDay monthDays
-  where
-    rightWeekDay day = let (_, _, wdAsInt) = toWeekDate day
-                       in wdAsInt == fromEnum weekday
-    monthDays = map (fromGregorian year month)
-                    [1 .. gregorianMonthLength year month]
+meetupDay schedule weekday year month = case schedule of
+  Teenth ->
+      firstWeekdayFrom weekday $ fromGregorian year month 13
+  Last ->
+      firstWeekdayFrom weekday beginLastWeek
+      where beginLastWeek = fromGregorian year month (monthLength - 6)
+            monthLength = gregorianMonthLength year month
+  nth ->
+      addDays (7 * asIntegral nth) firstWeekday
+      where firstWeekday = firstWeekdayFrom weekday $ fromGregorian year month 1
+
+firstWeekdayFrom :: Weekday -> Day -> Day
+firstWeekdayFrom weekday day = addDays (diff `mod` 7) day
+  where diff = asIntegral weekday - asIntegral (weekdayOn day)
+
+weekdayOn :: Day -> Weekday
+weekdayOn day = let (_, _, weekdayAsInt) = toWeekDate day
+                in toEnum weekdayAsInt
+
+asIntegral :: (Enum a, Integral b)  => a -> b
+asIntegral = fromIntegral . fromEnum
