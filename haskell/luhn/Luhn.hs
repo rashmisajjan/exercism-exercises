345b84beaa0254b3aeaf93f882a89dbc009ab4a8
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-17 22:53:20 +0200

    haskell/luhn: First working solution.

---
+++ haskell/luhn/Luhn.hs
@@ -0,0 +1,28 @@
+module Luhn (checkDigit, addends, checksum, isValid, create) where
+
+checkDigit :: Integer -> Integer
+checkDigit = (`rem` 10)
+
+addends :: Integer -> [Integer]
+addends = reverse . addends' . revDigits
+  where addends' []       = []
+        addends' [x]      = [x]
+        addends' (x:y:ys) = x : double' y : addends' ys
+        double' x | x > 4     = 2 * x - 9
+                  | otherwise = 2 * x
+
+checksum :: Integer -> Integer
+checksum = (`rem` 10) . sum . addends
+
+isValid :: Integer -> Bool
+isValid = (== 0) . checksum
+
+create :: Integer -> Integer
+create n = 10 * n + d
+  where d = (10 - checksum') `rem` 10
+        checksum' = checksum (10 * n)
+
+revDigits :: Integer -> [Integer]
+revDigits n | n < 10    = [n]
+            | otherwise = r : revDigits q
+  where (q, r) = n `quotRem` 10
