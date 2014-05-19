6fa191201099aa162cba6bb0ec4dbc1b7737ca9b
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-19 23:50:37 +0200

    go/word-count: Faster and simpler

--- go/word-count/wc.go
+++ go/word-count/wc.go
@@ -8,7 +8,7 @@ import (
 func WordCount(s string) Histogram {
 	counts := make(Histogram)
 	for _, word := range strings.FieldsFunc(s, func(r rune) bool {
-		return !unicode.In(r, unicode.Letter, unicode.Digit)
+		return !unicode.IsLetter(r) && !unicode.IsDigit(r)
 	}) {
 		counts[strings.ToLower(word)]++
 	}
