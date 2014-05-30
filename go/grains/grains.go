a70ea1192d6b84c7855fe45ef6b1ea47371947ff
Simon Jakobi <simon.jakobi@gmail.com>
2014-05-30 19:28:22 +0200

    go/grains: Update to comply with new spec

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -8,14 +8,19 @@
 
 package grains
 
+import "fmt"
+
 // Square returns the number of grains on square n of the chess board.
-func Square(n int) uint64 {
-	return 1 << uint(n-1)
+func Square(n int) (uint64, error) {
+	if n < 1 || n > 64 {
+		return 0, fmt.Errorf("grains.Square(%d): input is out of range [1..64]", n)
+	}
+	return 1 << uint(n-1), nil
 }
 
 // Total returns the total number of grains on the chessboard.
 func Total() uint64 {
 	// Total() is the sum of all powers of 2 with exponents 0 to 63.
-	// That sum is equal to 2^64 - 1.
-	return (1 << 64) - 1
+	// That sum is a 64 bit integer with all bits set to 1.
+	return ^uint64(0)
 }
