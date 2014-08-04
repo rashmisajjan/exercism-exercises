01cc198f698edca99f4cf39a437ea196c0b314aa
Simon Jakobi <simon.jakobi@gmail.com>
2014-08-04 22:02:17 +0200

    go/allergies: Improve performance with binary &

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,34 +1,34 @@
 package allergies
 
-var allergens = map[int]string{
-	1:   "eggs",
-	2:   "peanuts",
-	4:   "shellfish",
-	8:   "strawberries",
-	16:  "tomatoes",
-	32:  "chocolate",
-	64:  "pollen",
-	128: "cats",
+var allergens = []string{
+	"eggs",
+	"peanuts",
+	"shellfish",
+	"strawberries",
+	"tomatoes",
+	"chocolate",
+	"pollen",
+	"cats",
 }
 
+// Allergies returns the allergies that a person with a given allergy score
+// is allergic to.
 func Allergies(score int) []string {
 	result := make([]string, 0)
-	for i := 1; score > 0; i *= 2 {
-		if score%2 == 1 {
-			if allergen, ok := allergens[i]; ok {
-				result = append(result, allergen)
-			}
+	for i, allergen := range allergens {
+		if score&(1<<uint(i)) != 0 {
+			result = append(result, allergen)
 		}
-		score >>= 1
 	}
 	return result
 }
 
+// AllergicTo checks whether a given allergy score implies an allergy to
+// a given allergy.
 func AllergicTo(score int, allergen string) bool {
-	allergies := Allergies(score)
-	for _, allergy := range allergies {
-		if allergy == allergen {
-			return true
+	for i, a := range allergens {
+		if a == allergen {
+			return score&(1<<uint(i)) != 0
 		}
 	}
 	return false
