79baabe1c1cef60b957a6be9b128e4871534a8c7
Simon Jakobi <simon.jakobi@gmail.com>
2014-08-04 23:40:49 +0200

    python/octal: Implement new spec

--- python/octal/README.md
+++ python/octal/README.md
@@ -2,9 +2,6 @@
 
 Write a program that will convert a octal number, represented as a string (e.g. '1735263'), to its decimal equivalent using first principles (i.e. no, you may not use built-in ruby libraries or gems to accomplish the conversion).
 
-The program should consider strings specifying an invalid octal as the value 0.
-
-Tests are provided, delete one `skip` at a time.
 
 
 ## Source
--- python/octal/octal.py
+++ python/octal/octal.py
@@ -1,13 +1,7 @@
-class Octal(object):
-    def __init__(self, octal_string):
-        self.digits = self.__validate(octal_string)
-
-    def __validate(self, s):
-        for char in s:
-            if not '0' <= char < '8':
-                raise ValueError("Invalid octal digit: " + char)
-        return s
-
-    def to_decimal(self):
-        return sum(int(digit) * 8 ** i
-                   for (i, digit) in enumerate(reversed(self.digits)))
+def parse_octal(digits):
+    result = 0
+    for i, digit in enumerate(reversed(digits)):
+        if not '0' <= digit < '8':
+            raise ValueError("Invalid octal digit: " + digit)
+        result += int(digit) * 8 ** i
+    return result
