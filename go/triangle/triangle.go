20a73e71911c59a9a9c38f0d8b2f70a43d2ab157
Simon Jakobi <simon.jakobi@gmail.com>
2014-09-27 14:40:12 +0200

    go/triangle: More efficient implementation

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,10 +1,5 @@
 package triangle
 
-import (
-	"math"
-	"sort"
-)
-
 // A Kind denotes a type of triangle.
 type Kind uint8
 
@@ -19,22 +14,12 @@ const (
 // KindFromSides returns the Kind of the triangle defined by the lengths
 // of its sides a, b and c.
 func KindFromSides(a, b, c float64) Kind {
-	sides := []float64{a, b, c}
-
-	// Return NaT if any of the sidelengths are invalid.
-	for _, n := range sides {
-		if n <= 0 || math.IsInf(n, 1) || math.IsNaN(n) {
-			return NaT
-		}
-	}
-	sort.Float64s(sides)
-
 	switch {
-	case sides[0]+sides[1] <= sides[2]:
+	case !(a+b > c && a+c > b && b+c > a):
 		return NaT
 	case a == b && b == c:
 		return Equ
-	case sides[0] == sides[1] || sides[1] == sides[2]:
+	case a == b || a == c || b == c:
 		return Iso
 	default:
 		return Sca
