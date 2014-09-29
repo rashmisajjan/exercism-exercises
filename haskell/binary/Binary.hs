dcc17cdd5f554e1b176ea85ad2605e2e1bb283e5
Simon Jakobi <simon.jakobi@gmail.com>
2014-09-29 15:14:15 +0200

    haskell/binary: Relax return type from Integral a to Num a

--- haskell/binary/Binary.hs
+++ haskell/binary/Binary.hs
@@ -3,7 +3,7 @@ module Binary where
 import Data.List (foldl')
 import Data.Maybe (fromMaybe)
 
-toDecimal :: Integral a => String -> a
+toDecimal :: Num a => String -> a
 toDecimal = fromMaybe 0 . foldl' go (Just 0)
   where go (Just n) '0' = Just (2 * n)
         go (Just n) '1' = Just (2 * n + 1)
